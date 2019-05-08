ec0e6f90bfb6ef869edbf46370a8cafe578ae042
Dongwoo Pae <dongwoopae@gmail.com>
2019-05-07 20:21:26 -0700

    set up colored vcs with unwind segue

---
+++ Screens (iOSPT1)/RedViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RedViewController.swift
+//  Screens (iOSPT1)
+//
+//  Created by Dongwoo Pae on 5/7/19.
+//  Copyright © 2019 Dongwoo Pae. All rights reserved.
+//
+
+import UIKit
+
+class RedViewController: NumberedViewController {
+
+  //Unwind Segue
+    @IBAction func unwindSegueToRed(_ sender: UIStoryboardSegue) {}
+}
